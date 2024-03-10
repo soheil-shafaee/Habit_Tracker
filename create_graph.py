a38271af72bd4a0df451f898add8e074e3a07003
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2024-03-10 22:26:26 +0330

    ADD create graph function

--- create_graph.py
+++ create_graph.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QRadioButton, QPushButton
 from PyQt5 import uic
 import sys
-
+from add_pixel import AddPixel
 
 class CreateGraph(QMainWindow):
     def __init__(self):
@@ -11,7 +11,18 @@ class CreateGraph(QMainWindow):
 
         self.setFixedSize(436, 579)
 
+        self.pixel_page = None
+
+        self.create_graph_button = self.findChild(QPushButton, "create_graph")
+
+        self.create_graph_button.clicked.connect(self.graph_created)
+
         self.show()
+
+    def graph_created(self):
+        self.pixel_page = AddPixel()
+        self.pixel_page.show()
+        self.close()
 
 
 if __name__ == "__main__":
