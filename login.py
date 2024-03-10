ff09d7246a23ee6d5314a65c9c3a54d4b86589ee
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2024-03-11 03:05:34 +0330

    ADD Database into login file

--- login.py
+++ login.py
@@ -1,8 +1,28 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit, QCommandLinkButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit, QCommandLinkButton, QMessageBox
 from PyQt5 import uic
 import sys
 from signup import SignUp
 from add_pixel import AddPixel
+import psycopg2
+
+# ----------- DataBase Section --------
+try:
+    conx_add_graph = psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        password="8VMNKEZlg%",
+        database="habit_tracker",
+
+    )
+
+    cur = conx_add_graph.cursor()
+    com_users = """SELECT username, token FROM users"""
+    cur.execute(com_users)
+    users = cur.fetchall()
+
+
+except Exception as e:
+    print(e)
 
 
 class Login(QMainWindow):
@@ -16,8 +36,11 @@ class Login(QMainWindow):
         self.signup = None
         self.go_add_pixel = None
 
+        """Define our widgets"""
         self.login_button = self.findChild(QPushButton, "login")
         self.link_register = self.findChild(QCommandLinkButton, "register_2")
+        self.username_login = self.findChild(QLineEdit, "username")
+        self.password_login = self.findChild(QLineEdit, "password")
 
         self.login_button.clicked.connect(self.into_add_pixel)
         self.link_register.clicked.connect(self.register)
@@ -30,9 +53,16 @@ class Login(QMainWindow):
         self.close()
 
     def into_add_pixel(self):
-        self.go_add_pixel = AddPixel()
-        self.go_add_pixel.show()
-        self.close()
+        if self.username_login.text() == users[0][0] and self.password_login.text() == users[0][1]:
+            self.go_add_pixel = AddPixel()
+            self.go_add_pixel.show()
+            self.close()
+        else:
+            msg_login = QMessageBox()
+            msg_login.setWindowTitle("User Not found")
+            msg_login.setText("This user is not exist")
+            msg_login.setIcon(QMessageBox.Warning)
+            msg_login.exec_()
 
 
 if __name__ == "__main":
